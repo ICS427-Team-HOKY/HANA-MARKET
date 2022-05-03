0e00eedb6da1318dd7ab5e63c9bf53bf06ca9272
keithokuna <keithokuna@gmail.com>
2022-05-03 03:45:49 -0700

    removed insecure able to add and edit items

--- app/imports/ui/components/UserItem.jsx
+++ app/imports/ui/components/UserItem.jsx
@@ -3,6 +3,7 @@ import { Image, Card, Feed, Icon, Button } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withRouter, Link } from 'react-router-dom';
 import swal from 'sweetalert';
+import { itemsRemoveItMethod } from '../../api/item/Item';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 export class UserItem extends React.Component {
@@ -17,7 +18,7 @@ export class UserItem extends React.Component {
     })
       .then((willDelete) => {
         if (willDelete) {
-          this.props.Items.collection.remove(docID);
+          itemsRemoveItMethod.call(docID);
           swal('Delete successful', {
             icon: 'success',
           });
